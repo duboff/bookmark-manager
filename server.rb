df739bbd7599da1e3e79a0357d91fcc57604d63c
Mikhail Dubov <mdubov@gmail.com>
2014-03-24 19:06:17 +0000

    Can now add links

--- server.rb
+++ server.rb
@@ -14,5 +14,13 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 get '/' do
+  @links = Link.all
   erb :index
+end
+
+post '/links' do
+  url = params[:url]
+  title = params[:title]
+  Link.create(:url => url, :title => title)
+  redirect to('/')
 end
---
+++ spec/features/listing_all_links_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+feature "User browses the list of links" do
+  before(:each) {
+    Link.create(:title => "Makers Academy",
+                :url => "http://www.makersacademy.com")
+  }
+
+  scenario "when opening the hom page" do
+    visit '/'
+    expect(page).to have_content("Makers Academy")
+  end
+
+end
+
+feature "User adds a new link" do
+
+  scenario "when browsing the homepage" do
+    expect(Link.count).to eq 0
+    visit '/'
+    add_link("http://www.makersacademy.com/", "Makers Academy")
+    expect(Link.count).to eq 1
+    link = Link.first
+    expect(link.url).to eq "http://www.makersacademy.com/"
+    expect(link.title).to eq "Makers Academy"
+  end
+
+  def add_link(url, title)
+    within('#new-link') do
+      fill_in 'url', :with => url
+      fill_in 'title', :with => title
+      click_button 'Add link'
+    end
+  end
+end
